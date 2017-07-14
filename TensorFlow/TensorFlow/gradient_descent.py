a8a9978d37b58b9e5fd5d01132d969f640e49c92
antonio.lourenco <antonio.lourenco@vision-box.com>
2017-07-15 00:37:58 +0100

    Introduction to TensorFlow

---
+++ TensorFlow/TensorFlow/gradient_descent.py
@@ -0,0 +1,51 @@
+# required imports
+import numpy as np
+import tensorflow as tf
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+
+# define some parameters
+n_epochs = 1000
+learning_rate = 0.01
+
+# clean graph
+reset_graph()
+
+# read and debug data dimenions
+housing = fetch_california_housing()
+m,n  = housing.data.shape
+
+# Scale the data  for better conditioning of the problem
+# concatenate housing price with ones at the begining.
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+
+# batch gradient descent optimization
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients = 2/m * tf.matmul(tf.transpose(X), error)
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
